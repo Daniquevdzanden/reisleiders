798a815c53bab022ad4307d97bdd440dd926fbc3
Daniquevdzanden <109664915+Daniquevdzanden@users.noreply.github.com>
2026-01-02 12:38:53 +0100

    sidenav aanpassingen

--- components/Sidenav.js
+++ components/Sidenav.js
@@ -297,6 +297,23 @@ export default function Sidenav({ children }) {
                     />
                   </span>
                 </MenuButton>
+                <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-xl bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none">
+                  {userNavigation.map((item) => (
+                    <MenuItem key={item.name}>
+                      {({ active }) => (
+                        <Link
+                          href={item.href}
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          {item.name}
+                        </Link>
+                      )}
+                    </MenuItem>
+                  ))}
+                </MenuItems>
               </Menu>
             </div>
           </div>
